c66386bacb24bc9518d080e52b0818dde206b2a8
Jarvis <jarvis@metzler.dev>
2026-07-21 07:57:20 +0000

    feat: add ArgoCD (Helm) + app-of-apps structure + migrate kafka broker config to GitOps

---
+++ terraform/argocd.tf
@@ -0,0 +1,15 @@
+# ----- ArgoCD -----
+resource "helm_release" "argocd" {
+  name             = "argocd"
+  repository       = "https://argoproj.github.io/argo-helm"
+  chart            = "argo-cd"
+  namespace        = "argocd"
+  create_namespace = true
+
+  set {
+    name  = "server.service.type"
+    value = "ClusterIP"
+  }
+
+  depends_on = [azurerm_kubernetes_cluster.main]
+}
